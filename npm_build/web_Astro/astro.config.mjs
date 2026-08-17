b1d0cb19b659bc47de3bcc467452782c8d802d50
Lía Da Silva <lia@bakerlab.cl>
2026-08-17 16:29:30 -0400

    wipe-out

--- npm_build/web_Astro/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  //---CAMBIAR A URL DE DOMINIO---//
-  site: 'https://lia-bakerlab.github.io',
-  //---FIN URL DE DOMINIO---//
-  output: 'static',
-});
